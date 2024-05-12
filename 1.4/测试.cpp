536e2043bbef1511c1d0467ea69eae9fe366b5e1
2070super <1025404072@qq.com>
2024-05-12 23:28:13 +0800

    23:27

--- 1.4/测试.cpp
+++ 1.4/测试.cpp
@@ -1,8 +1,9 @@
-#include <bit/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 int main() {
     int a, b;
     cin >> a >> b;
     cout << a + b << endl;
+	cout << a - b << endl;
     return 0;
 }

89ee7e353cd7a1c83686a27d924dfcd2280719eb
Capzera <1786126188@qq.com>
2024-05-12 23:28:23 +0800

    23:28

--- 1.4/测试.cpp
+++ 1.4/测试.cpp
@@ -4,5 +4,10 @@ int main() {
     int a, b;
     cin >> a >> b;
     cout << a + b << endl;
+	int sum = 1;
+	for (int i = 0; i < b; i++) {
+		sum *= a;
+	}
+	cout << "Pow(a, b) = " << sum << endl;
     return 0;
 }

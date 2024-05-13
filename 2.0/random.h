fc40c55ac85afcaeda8f42d3d9caac92f96a91ef
Capzera <1786126188@qq.com>
2024-05-13 13:05:17 +0800

    synchronization
    Signed-off-by: Capzera <1786126188@qq.com>

--- 2.0/random.h
+++ 2.0/random.h
@@ -165,10 +165,10 @@ string highIntRand(string Left, string Right) {
 	for (size_t i = 0; i < Left.size(); i++) {
 		int anum = Left[i] - '0';
 		int bnum = Right[i] - '0';
-		if(anum>bnum)swap(anum,bnum);
 		answer.push_back(mrand(anum, bnum) + '0');
 	}
 	size_t a = 0;
+	// bug
 	while (answer[a] == '0') {
 		a++;
 	}
